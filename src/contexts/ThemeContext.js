dd2dfd88639f9cc96372d319400905357cf15c93
Antonio Velazquez <31423917+Antonio072@users.noreply.github.com>
2020-09-11 00:01:30 -0500

    Save current theme (dark or light mode)

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,13 +1,27 @@
-import React, { createContext, useState } from 'react';
+import React, { useEffect, createContext, useState } from 'react';
 
 export const ThemeContext = createContext()
 
 function ThemeContextProvider(props) {
-    var [state, setState] = useState({
-        isLightTheme: true,
-        light: { syntax: '#555', ui: '#ddd', bg: '#eee', btn: 'btn shadow hover:bg-white hover:text-gray-600 rounded h-10 px-3 mx-3 bg-gray-600 text-white' },
-        dark: { syntax: '#ddd', ui: '#333', bg: '#555',  btn: 'btn shadow hover:bg-gray-600 hover:text-white rounded h-10 px-3 mx-3  bg-white text-gray-600' }
-    })
+    var [state, setState] = useState(
+        () => {
+            const theme = localStorage.getItem('theme')
+            return theme ? JSON.parse(theme): 
+            {
+                isLightTheme: true,
+                light: { syntax: '#555', ui: '#ddd', bg: '#eee', btn: 'btn shadow hover:bg-white hover:text-gray-600 rounded h-10 px-3 mx-3 bg-gray-600 text-white' },
+                dark: { syntax: '#ddd', ui: '#333', bg: '#555', btn: 'btn shadow hover:bg-gray-600 hover:text-white rounded h-10 px-3 mx-3  bg-white text-gray-600' }
+            }
+        }
+    )
+
+    useEffect(
+        () => {
+            localStorage.setItem('theme', JSON.stringify(state))
+        },
+        [state]
+    )
+
 
     var toggleTheme = () => {
         document.body.classList.add('dark');
